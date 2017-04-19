17b7183544008f56cc846e955af614d8fe40b64e
dideb001 <dideb001@cougars.csusm.edu>
2017-04-19 14:56:17 -0700

    Add files via upload

--- app/src/main/java/com/csusm/didhofsas/wtfis/CountryActivity.java
+++ app/src/main/java/com/csusm/didhofsas/wtfis/CountryActivity.java
@@ -5,12 +5,13 @@ import android.app.ActivityManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 //Read the countrys, Return: both countrys as Integer
 //0 = US
@@ -23,39 +24,28 @@ public class CountryActivity extends Activity implements View.OnClickListener {
 
     Spinner homeSpinner, travelSpinner;
     Button submitButton;
+    Integer[] imagearray = {R.drawable.us_temp, R.drawable.uk_temp, R.drawable.eu_temp};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
-
-        CountrycodeDataSource db = new CountrycodeDataSource(this);
-        db.open();
+        String [] textArray = this.getIntent().getExtras().getStringArray("country_array");
 
         homeSpinner = (Spinner)findViewById(R.id.spinner_Homecountry);
         travelSpinner = (Spinner)findViewById(R.id.spinner_Travelcountry);
         submitButton = (Button)findViewById(R.id.cButton);
         submitButton.setOnClickListener(this);
 
-        ArrayAdapter homeSpinnerArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item,
-                db.selectNamesInTable(CountrycodeDBHelper.COUNTRY_TABLE, CountrycodeDataSource.COUNTRY_COLUMNS));
-        homeSpinner.setAdapter(homeSpinnerArrayAdapter);
-        homeSpinner.setSelection(db.selectLastHome());
-
-        ArrayAdapter travelSpinnerArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item,
-                db.selectNamesInTable(CountrycodeDBHelper.COUNTRY_TABLE, CountrycodeDataSource.COUNTRY_COLUMNS));
-        travelSpinner.setAdapter(travelSpinnerArrayAdapter);
-        travelSpinner.setSelection(db.selectLastTravel());
-        db.close();
+        SpinnerAdapter adapter = new SpinnerAdapter(this, android.R.layout.simple_spinner_dropdown_item, textArray, imagearray);
+        homeSpinner.setAdapter(adapter);
+        travelSpinner.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View v)
     {
         Intent i = new Intent(this, MainActivity.class);
-        Log.i("INTENT","travelSpinner selected Postion: " + travelSpinner.getSelectedItemPosition() + ", travelSpinner selected ID" + travelSpinner.getSelectedItemId());
         i.putExtra("homeCode", homeSpinner.getSelectedItemPosition());
         i.putExtra("travelCode", travelSpinner.getSelectedItemPosition());
         setResult(0, i);
